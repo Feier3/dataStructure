1a5e32f5a10a30adb06dbbf6a6fda069ca47dbc4
陈亚飞 <feiyafei33@163.com>
2020-03-11 23:23:12 +0800

    '快速排序

--- DS3-sortandsearch/sortDemo.py
+++ DS3-sortandsearch/sortDemo.py
@@ -66,24 +66,149 @@
 希尔排序
 """
 
+# def shell_sort(arr):
+#     import math
+#     gap = 1
+#     while (gap < len(arr) / 3):
+#         gap = gap * 3 + 1
+#         print(gap)
+#     while gap > 0:
+#         for i in range(gap, len(arr)):
+#             temp = arr[i]
+#             j = i - gap
+#             while j >= 0 and arr[j] > temp:
+#                 arr[j + gap] = arr[j]
+#                 j -= gap
+#             arr[j + gap] = temp
+#         gap = math.floor(gap / 3)
+#     return arr
+#
+#
+# alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+# print(shell_sort(alist))
 
-def shell_sort(arr):
-    import math
-    gap = 1
-    while (gap < len(arr) / 3):
-        gap = gap * 3 + 1
-        print(gap)
-    while gap > 0:
-        for i in range(gap, len(arr)):
-            temp = arr[i]
-            j = i - gap
-            while j >= 0 and arr[j] > temp:
-                arr[j + gap] = arr[j]
-                j -= gap
-            arr[j + gap] = temp
-        gap=math.floor(gap/3)
-    return arr
+"""
+快速排序
+"""
 
 
-alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-print(shell_sort(alist))
+# def quick_sort(lst):
+#     qsort_rec(lst, 0, len(lst) - 1)
+#     return lst
+#
+# def qsort_rec(lst, l, r):
+#     if l >= r:
+#         return  # 分段无记录或只有一个记录
+#     i = l
+#     j = r
+#     pivot = lst[i]  # lst[i]是初始空位
+#     while i < j:  # 找pivot的最终位置
+#         while i < j and lst[j] >= pivot:
+#             j -= 1  # 用j向左扫描找小于pivot的记录
+#         if i < j:
+#             lst[i] = lst[j]
+#             i += 1  # 小记录移到左边
+#         while i < j and lst[i] <= pivot:
+#             i += 1  # 用i向右扫描找大于pivot的记录
+#         if i < j:
+#             lst[j] = lst[i]
+#             j -= 1  # 大记录移到右边
+#     lst[i] = pivot  # 将pivot存入其最终位置
+#     qsort_rec(lst, l, i - 1)  # 递归处理左半区间
+#     qsort_rec(lst, i + 1, r)  # 递归处理右半区间
+#
+#
+# alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+# print(quick_sort(alist))
+
+# def quick_sort1(lst):
+#     def qsort(lst, begin, end):
+#         if begin >= end:
+#             return
+#         pivot = lst[begin]
+#         i = begin
+#         for j in range(begin + 1, end + 1):
+#             if lst[j] < pivot:  # 发现一个小元素
+#                 i += 1
+#                 lst[i], lst[j] = lst[j], lst[i]  # 小元素就位
+#         lst[begin], lst[i] = lst[i], lst[begin]  # 枢轴元就位
+#         qsort(lst, begin, i - 1)
+#         qsort(lst, i + 1, end)
+#
+#     qsort(lst, 0, len(lst) - 1)
+#     return lst
+#
+#
+# alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+# print(quick_sort1(alist))
+
+
+# def quickSort(arr, left=None, right=None):
+#     left = 0 if not isinstance(left, (int, float)) else left
+#     right = len(arr) - 1 if not isinstance(right, (int, float)) else right
+#     if left < right:
+#         partitionIndex = partition(arr, left, right)
+#         quickSort(arr, left, partitionIndex - 1)
+#         quickSort(arr, partitionIndex + 1, right)
+#     return arr
+#
+#
+# def partition(arr, left, right):
+#     pivot = left
+#     index = pivot + 1
+#     i = index
+#     while i <= right:
+#         if arr[i] < arr[pivot]:
+#             swap(arr, i, index)
+#             index += 1
+#         i += 1
+#     swap(arr, pivot, index - 1)
+#     return index - 1
+
+
+# def swap(arr, i, j):
+#     arr[i], arr[j] = arr[j], arr[i]
+#
+#
+# alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+# print(quickSort(alist))
+
+
+'''
+    归并排序
+'''
+def mergeSort(alist):
+    print('拆分的列表',alist)
+    if len(alist) > 1:
+        mid = len(alist)//2
+        leftHalf = alist[:mid]
+        rightHalf = alist[mid:]
+
+        mergeSort(leftHalf)
+        mergeSort(rightHalf)
+
+        i = 0
+        j = 0
+        k = 0
+        while i < len(leftHalf) and j < len(rightHalf):
+            if leftHalf[i] < rightHalf[j]:
+                alist[k] = leftHalf[i]
+                i = i + 1
+            else:
+                alist[k] = rightHalf[j]
+                j = j + 1
+
+            k = k + 1
+        while i < len(leftHalf):
+            alist[k] = leftHalf[i]
+            i = i + 1
+            k = k + 1
+        while j < len(rightHalf):
+            alist[k] = rightHalf[j]
+            j = j + 1
+            k = k + 1
+    print('合并：',alist)
+
+alist = [54,26,93,17,77,31,44,55,20]
+mergeSort(alist)
+print(alist)
